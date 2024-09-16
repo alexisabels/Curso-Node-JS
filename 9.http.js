f9107c1102a39d71d291086495c4ea2d0adb9073
alexisabels <isabelsanchezalex@gmail.com>
2024-09-16 23:44:10 +0200

    http port auto setter

--- 9.http.js
+++ 9.http.js
@@ -1,12 +1,12 @@
 const http = require("node:http");
-
+const { findAvailablePort } = require("./10.free-port.js");
 const server = http.createServer((req, res) => {
   console.log("Request received");
   res.end("Hola mundo");
 });
 
-server.listen(0, () => {
-  console.log(
-    `server escuchando en puerto http://localhost:${server.address().port}`
-  );
+findAvailablePort(1234).then((port) => {
+  server.listen(port, () => {
+    console.log(`server escuchando en puerto http://localhost:${port}`);
+  });
 });
