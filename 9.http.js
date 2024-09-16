fab180859b4ed3c46dd4f3e44301207fe7523e0d
alexisabels <isabelsanchezalex@gmail.com>
2024-09-17 00:19:11 +0200

    gitignore

--- 9.http.js
+++ 9.http.js
@@ -1,11 +1,12 @@
 const http = require("node:http");
 const { findAvailablePort } = require("./10.free-port.js");
+const desiredPort = process.env.PORT ?? 3000;
 const server = http.createServer((req, res) => {
   console.log("Request received");
   res.end("Hola mundo");
 });
 
-findAvailablePort(1234).then((port) => {
+findAvailablePort(desiredPort).then((port) => {
   server.listen(port, () => {
     console.log(`server escuchando en puerto http://localhost:${port}`);
   });
