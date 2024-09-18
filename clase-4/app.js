dea560bba78045a2fd16750d972adcfc55bc589d
alexisabels <isabelsanchezalex@gmail.com>
2024-09-19 00:37:54 +0200

    clase 4

---
+++ clase-4/app.js
@@ -0,0 +1,20 @@
+import express, { json } from 'express'
+
+import { moviesRouter } from './routes/movies.js'
+import { corsMiddleware } from './middlewares/cors.js'
+
+const app = express()
+
+app.use(json())
+
+app.use(corsMiddleware)
+
+app.disable('x-powered-by')
+
+app.use('/movies', moviesRouter)
+
+const PORT = process.env.PORT ?? 1234
+
+app.listen(PORT, () => {
+  console.log('Escuchando en https://localhost.com:1234')
+})
